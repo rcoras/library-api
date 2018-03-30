273d11419ce4558e5807df44035ff37352bd4221
rcoras <rcoras@yahoo.com>
2018-03-30 09:01:02 -0400

    Adds comments
    Adds a couple comments to clarify that comments table is not currently in use in the front end but available for future updates

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
+# relationship to comments included for future update to front end to add comments.
+# comments will be destroyed when their associated book is destroyed
+
 class Book < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
+# added for future update to front end for users to add comments to books
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :book
